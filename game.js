9c0634474496414c7483b06781b7d5e5817e1ffc
Philip Jun <inbox@philipjun.com>
2014-04-28 11:58:01 -0400

    adding circles

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   var circles =[];
     for(var i=0, i< 10, i++){
-      
-      
+      var circle = new Circle();
+      circles.push(circle);
+      circles[i].render();
+
     }
 
 
@@ -21,3 +23,32 @@ $(document).ready(function(){
 
 });
 
+function Circle() {
+  this.x = Math.random() * 450;
+  this.y= Math.random() * 450;
+  this.diameter= 30 + Math.random() * 50;
+  this.speed= 500 + Math.random() * 1500;
+
+  this.render = function(){
+    this.$me = $('div class="circle"></div>').css('left, this.x')
+    .css('top', this.y)
+    .css('width', this.diameter)
+    .css('height', this.diameter);
+
+    $('game').append(this.$me);
+  
+  };
+
+  this.move = function(){
+    this.$me.animate({
+      top: "+=300px",
+    left: "+=300px",
+
+    }, {
+      complete: function(){
+        this.move();
+      }
+    });
+  };
+
+}
