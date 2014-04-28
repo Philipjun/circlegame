c2f90b134ed088e4a9b4e2c004efbc1dae7c4c8a
Philip Jun <inbox@philipjun.com>
2014-04-28 11:32:58 -0400

    adding circles

--- game.js
+++ game.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+  var circles =[];
+    for(var i=0, i< 10, i++){
+      
+      
+    }
+
+
+
+  $('circle').animate({
+    top: "+=300px",
+    left: "+=300px",
+
+  }, {
+    complete: function() {
+      alert("I moved!");
+    }
+  });
 
 
 
